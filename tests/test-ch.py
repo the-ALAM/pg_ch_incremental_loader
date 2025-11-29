e2af30486daaf3a93db9e36422380930436c2674
the-alam <dev.alameldeen@gmail.com>
2025-11-29 03:20:34 +0200

    mods to the ch test and project deps

--- tests/test-ch.py
+++ tests/test-ch.py
@@ -1,28 +1,30 @@
-import os
-from clickhouse_driver import Client
+import clickhouse_connect
 from dotenv import load_dotenv
+import os
 
 def test_clickhouse_connection():
     load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), 'test.env'))
 
-    ch_host = os.environ.get("CLICKHOUSE_HOST", "localhost")
-    ch_port = int(os.environ.get("CLICKHOUSE_PORT", "9000"))
-    ch_user = os.environ.get("CLICKHOUSE_USER", "default")
-    ch_password = os.environ.get("CLICKHOUSE_PASSWORD", "")
-    ch_database = os.environ.get("CLICKHOUSE_DB", "default")
+    ch_host = os.environ.get("CLICKHOUSE_HOST")
+    ch_port = int(os.environ.get("CLICKHOUSE_PORT"))
+    ch_database = os.environ.get("CLICKHOUSE_DB")
+    ch_user = os.environ.get("CLICKHOUSE_USER")
+    ch_password = os.environ.get("CLICKHOUSE_PASSWORD")
 
     try:
-        client = Client(
+        client = clickhouse_connect.get_client(
             host=ch_host,
             port=ch_port,
-            user=ch_user,
+            username=ch_user,
             password=ch_password,
             database=ch_database
         )
-        result = client.execute('SELECT 1')
+        result = client.command('SELECT True;')
         print("Connection successful. Query result:", result)
     except Exception as e:
         print("Failed to connect or execute query:", e)
 
 if __name__ == "__main__":
+    print("Testing ClickHouse connection...")
     test_clickhouse_connection()
+    print("ClickHouse connection test completed.")
