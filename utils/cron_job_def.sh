052e2f83ce934c6963959971882bd7b87ad29ee6
the-alam <dev.alameldeen@gmail.com>
2025-11-30 20:43:59 +0200

    changed the job frequency back to every 30 minutes

--- utils/cron_job_def.sh
+++ utils/cron_job_def.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CRON_JOB="*/1 * * * * cd /mnt/d/code/pg_ch_incremental_loader && /home/alam/.local/bin/uv run ./app/loader.py >> /mnt/d/code/pg_ch_incremental_loader/r/cron.log 2>&1"
+CRON_JOB="*/30 * * * * cd /mnt/d/code/pg_ch_incremental_loader && /home/alam/.local/bin/uv run ./app/loader.py >> /mnt/d/code/pg_ch_incremental_loader/r/cron.log 2>&1"
 CRON_FILE="/var/spool/cron/crontabs/alam"
 
 if ! grep -Fxq "$CRON_JOB" "$CRON_FILE"; then
