7abee9556b4078e63d3d74f526cb17876c889588
the-alam <dev.alameldeen@gmail.com>
2025-11-30 20:40:41 +0200

    job submitter shell script

--- utils/cron_job_def.sh
+++ utils/cron_job_def.sh
@@ -1,1 +1,8 @@
-*/30 * * * * /bin/bash uv run ./app/app.py >> .//cron.log 2>&1
+#!/bin/bash
+
+CRON_JOB="*/1 * * * * cd /mnt/d/code/pg_ch_incremental_loader && /home/alam/.local/bin/uv run ./app/loader.py >> /mnt/d/code/pg_ch_incremental_loader/r/cron.log 2>&1"
+CRON_FILE="/var/spool/cron/crontabs/alam"
+
+if ! grep -Fxq "$CRON_JOB" "$CRON_FILE"; then
+    echo "$CRON_JOB" >> "$CRON_FILE"
+fi
